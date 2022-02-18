c6a829148684ad8af1947f7965414e5e5295d0e0
Hulxv <m7mdx37@gmail.com>
2022-02-18 03:17:20 +0200

    [client;new] complete 'Home' page

--- client/layout/PageLayout.js
+++ client/layout/PageLayout.js
@@ -16,13 +16,14 @@ export function PageLayout({ children, isLoading }) {
 
 	return (
 		<motion.div
+			style={{ height: "100vh", width: "100vw" }}
 			variants={variants} // Pass the variant object into Framer Motion
 			initial='hidden' // Set the initial state to variants.hidden
 			animate='enter' // Animated state to variants.enter
 			exit='exit' // Exit state (used later) to variants.exit
 			transition={{ type: "linear" }} // Set the transition to linear
 		>
-			<Stack align='center' w='full'>
+			<Stack align='center' h='full'>
 				<Header />
 				{children}
 			</Stack>
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,18 @@
 import Head from "next/head";
 import { Loading } from "@Components";
+
+import { Stack, Button, Heading } from "@chakra-ui/react";
+
+import { useTheme } from "@Theme";
 export default function Home() {
-	return <div>Home</div>;
+	const { bg, bgHover, color } = useTheme();
+	return (
+		<Stack align='center' justify='center' h='full'>
+			<Stack spacing='-0.5' align='center'>
+				<Heading size='sm'>Welcome to</Heading>
+				<Heading textAlign={"center"}>Electronic Document System</Heading>
+			</Stack>
+			<Button maxW={100}>Templete</Button>
+		</Stack>
+	);
 }
