a60ce92d68f5d6403b27ca867251171236bdd2da
Hulxv <m7mdx37@gmail.com>
2022-03-25 17:36:10 +0200

    [new;client] add a placeholder for inputs in login page

--- client/pages/auth/login.js
+++ client/pages/auth/login.js
@@ -5,11 +5,14 @@ import {
 	Stack,
 	Button,
 	Input,
-	Heading,
 	HStack,
 	Box,
 	IconButton,
 	Tooltip,
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	FormHelperText,
 } from "@chakra-ui/react";
 
 import {
@@ -19,18 +22,42 @@ import {
 } from "react-icons/ai";
 import { BsKey } from "react-icons/bs";
 import { BiLogIn } from "react-icons/bi";
+
+function generatingRandomPassword() {
+	let chars =
+		"0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	let password = "";
+	for (let i = 0; i < Math.floor(Math.random() * 16) + 8; i++) {
+		password += chars[Math.floor(Math.random() * (chars.length - 1))];
+	}
+	return password;
+}
+
 export default function Login() {
 	const [info, setInfo] = useState({ username: "", password: "" });
 	const [isPassFieldVisiable, setIsPassFieldVisiable] = useState(false);
+	const [passwordPlaceholder, _] = useState(generatingRandomPassword());
 	return (
-		<Stack w='full' maxW={"96"} spacing={6} padding={4} align='center' h='full'>
+		<FormControl
+			as={Stack}
+			w='full'
+			maxW={"96"}
+			spacing={6}
+			padding={4}
+			align='center'
+			h='full'
+			justify={"center"}>
 			<Logo />
 			<Stack w='full' spacing={1}>
 				<HStack align='end'>
 					<AiOutlineUser size='1.3em' />
-					<Heading fontSize={13}>Username</Heading>
+					<FormLabel for='username' fontSize={13}>
+						Username
+					</FormLabel>
 				</HStack>
 				<Input
+					placeholder='username'
+					id='username'
 					variant={"filled"}
 					value={info.username}
 					onChange={(e) => setInfo({ ...info, username: e.target.value })}
@@ -39,7 +66,9 @@ export default function Login() {
 			<Stack w='full' spacing={1}>
 				<HStack align='end'>
 					<BsKey size='1.3em' />
-					<Heading fontSize={13}>Password</Heading>
+					<FormLabel htmlfor='password' fontSize={13}>
+						Password
+					</FormLabel>
 				</HStack>
 				<Box
 					position={"relative"}
@@ -47,7 +76,9 @@ export default function Login() {
 					display={"flex"}
 					alignContent='center'>
 					<Input
+						placeholder={passwordPlaceholder}
 						variant={"filled"}
+						id='password'
 						type={isPassFieldVisiable ? "text" : "password"}
 						value={info.password}
 						onChange={(e) => setInfo({ ...info, password: e.target.value })}
@@ -71,6 +102,6 @@ export default function Login() {
 				</Box>
 			</Stack>
 			<Button rightIcon={<BiLogIn size='1.4em' />}>Login</Button>
-		</Stack>
+		</FormControl>
 	);
 }
