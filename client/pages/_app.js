afa81d487f5ed57757fba727c9b3b908ed6f99a5
Hulxv <m7mdx37@gmail.com>
2022-06-10 18:52:43 +0200

    [client;feat] add title to pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
 import { PageLayout } from "@Layout/PageLayout";
 import { breakpoints } from "@Theme";
+import AuthProvider from "@Auth";
+
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { StepsStyleConfig as Steps } from "chakra-ui-steps";
-import AuthProvider from "@Auth";
+
 import "../styles/globals.css";
 
 const theme = extendTheme({
@@ -13,14 +18,37 @@ const theme = extendTheme({
 });
 
 function App({ Component, pageProps }) {
+	let router = useRouter();
+	console.log({ ...router });
+	let title = `EDS | ${(() => {
+		switch (router.pathname) {
+			case "/":
+				return "Home";
+			case "/404":
+				return "Not found";
+			default:
+				return router.pathname
+					.split("/")
+					.at(-1)
+					.split("")
+					.map((c, index) => (index === 0 ? c.toUpperCase() : c))
+					.join("");
+		}
+	})()} `;
+
 	return (
-		<ChakraProvider theme={theme}>
-			<AuthProvider>
-				<PageLayout>
-					<Component {...pageProps} />
-				</PageLayout>
-			</AuthProvider>
-		</ChakraProvider>
+		<>
+			<Head>
+				<title>{title}</title>
+			</Head>
+			<ChakraProvider theme={theme}>
+				<AuthProvider>
+					<PageLayout>
+						<Component {...pageProps} />
+					</PageLayout>
+				</AuthProvider>
+			</ChakraProvider>
+		</>
 	);
 }
 
