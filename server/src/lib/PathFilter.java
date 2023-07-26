803147b02de238db105ce1ab83058a99bfc026da
Morad <460583232@sharkia1.moe.edu.eg>
2023-07-26 12:12:16 +0300

    Readability

--- server/src/lib/PathFilter.java
+++ server/src/lib/PathFilter.java
@@ -1,16 +1,14 @@
 package lib;
 /**
  * HTTP Path filter to prevent LFI
- * @author morad
- *
+ * @author Morad A.
  */
 public class PathFilter {
 	public static String filter(String path) {
 		String res = path;
 		res = res.replaceAll("\\.\\.", ""); // LFI protection, i guess?
 		res = res.replaceAll("//", "/");
-		if (res.endsWith("/"))
-			res = res + "index.html";
+		if (res.endsWith("/")) res = res + "index.html";
 		return res;
 	}
 }
