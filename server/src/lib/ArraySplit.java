28983b8ac02335990d279bb9850cd4cdb3808a9c
Morad <460583232@sharkia1.moe.edu.eg>
2023-07-26 12:09:24 +0300

    Slight Performance Improvement

--- server/src/lib/ArraySplit.java
+++ server/src/lib/ArraySplit.java
@@ -1,7 +1,7 @@
 package lib;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 /**
  * See {@link #split(byte[], byte[])}
@@ -15,10 +15,11 @@ public class ArraySplit {
 	 * @return A list with the resulting arrays
 	 */
 	public static List<byte[]> split(byte[] array, byte[] delimiter) {
-		List<byte[]> byteArrays = new LinkedList<>();
+		List<byte[]> byteArrays = new ArrayList<>(array.length / delimiter.length + 1);
 		int begin = 0;
-
-		outer: for (int i = 0; i < array.length - delimiter.length + 1; i++) {
+		int end = array.length - delimiter.length + 1;
+	
+		outer: for (int i = 0; i < end; i++) {
 			for (int j = 0; j < delimiter.length; j++) {
 				if (array[i + j] != delimiter[j]) {
 					continue outer;
