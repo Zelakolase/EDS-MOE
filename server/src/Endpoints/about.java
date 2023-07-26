67ad7e88dbdb9823a84cdfd4b38c4517dcdf0f6b
Morad <460583232@sharkia1.moe.edu.eg>
2023-07-26 12:06:44 +0300

    Simplify function

--- server/src/Endpoints/about.java
+++ server/src/Endpoints/about.java
@@ -1,23 +1,23 @@
 package Endpoints;
 
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import lib.AES;
-import lib.IO;
 import lib.JSON;
 import lib.SparkDB;
 
+/**
+ * Class for API Endpoint '/api.about'
+ * @author Morad A.
+ */
 public class about {
-	public String run(String Key, SparkDB docs, AES aes) throws Exception{
-			HashMap<String, String> data = new HashMap<>();
-			data.put("query_num",aes.decrypt(new String(IO.read("./conf/queries.txt"))));
-			ArrayList<String> docnums = new ArrayList<>();
-			docnums = docs.getColumn("size"); // Get all sizes
-			int docnum = 0;
-			for(String temp : docnums) if(!temp.equals("0")) docnum += Integer.parseInt(temp);
-			data.put("document_num", String.valueOf(docnum));
-			return JSON.HMQ(data);
+	public String run(String Key, SparkDB metadata, AES aes) throws Exception {
+		HashMap<String, String> data = new HashMap<>();
+		long queryNumber = Long.valueOf(metadata.get(0).get("numQueries"));
+		long docNumber = Long.valueOf(metadata.get(0).get("numDocs"));
+		data.put("query_num", String.valueOf(queryNumber));
+		data.put("document_num", String.valueOf(docNumber));
+		return JSON.HMQ(data);
 	}
 }
