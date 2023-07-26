adea23bd507a7bc1374f1aa5b7fe9373387ec0c6
Morad <460583232@sharkia1.moe.edu.eg>
2023-07-26 12:08:19 +0300

    Bug Fix

--- server/src/Endpoints/name.java
+++ server/src/Endpoints/name.java
@@ -3,12 +3,13 @@ package Endpoints;
 import java.util.HashMap;
 
 import lib.JSON;
+import lib.SparkDB;
 
 public class name {
-	public String run(String ENCRYPTION_KEY, String SCHOOL) throws Exception{
+	public String run(String ENCRYPTION_KEY, SparkDB metadata) throws Exception{
 			return JSON.HMQ(new HashMap<String, String>() {
 				{
-					put("name", SCHOOL);
+					put("name", metadata.get(0).get("entityName"));
 				}
 			});
 	}
